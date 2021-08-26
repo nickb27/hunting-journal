4f0449093b11238b84d56d69c762f0fa2760690a
nickb27 <nick.brill@sentera.comt>
2021-08-26 15:56:55 -0500

    add buttons, css and state

--- hunting-journal/src/App.js
+++ hunting-journal/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`react-root ${color}`}>
+      <div className='centered'>
+        <h1>Hunting Journal</h1>
+        {color}
+        <button className='red' onClick={() => setColor('red')}>red</button>
+        <button className='blue' onClick={() => setColor('blue')}>blue</button>
+        <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
+      </div>
     </div>
   );
 }
---
+++ hunting-journal/src/ColorChangeButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CollorChangeButon() {
+  return (
+    <button>Hi!</button>
+  )
+}
+
+export default CollorChangeButon
